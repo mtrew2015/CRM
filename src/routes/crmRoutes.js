d4bfbcd6c5ec94cbe131a43c3932425a7347eb5b
Michael Trew <mtrew2015@gmail.com>
2022-02-16 11:54:39 -0500

    adding middleware placeholder

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,9 +1,18 @@
 const routes = (app) => {
   app
     .route('/contact')
-    .get((req, res) => {
-      res.send('GET Request successful in contact route');
-    })
+    .get(
+      (req, res, next) => {
+        // middleware
+        console.log(`Request from: ${req.originalUrl}`);
+        console.log(`Request type: ${req.method}`);
+        // calling next to pass to next function
+        next();
+      },
+      (req, res, next) => {
+        res.send('GET Request successful in contact route');
+      }
+    )
 
     .post((req, res) => {
       res.send('GET Request successful in contact route');
